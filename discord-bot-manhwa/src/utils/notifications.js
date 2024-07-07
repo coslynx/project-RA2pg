a0ba51e00d769f2e3e84c3c2e08367bbc57a71f2
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-07 18:11:52 +0530

    Generated: `discord-bot-manhwa/src/utils/notifications.js`

---
+++ discord-bot-manhwa/src/utils/notifications.js
@@ -0,0 +1,11 @@
+const notifications = {
+  notifyNewChapter: (userId, seriesId, chapterNumber) => {
+    // Notify user about the new chapter for the series
+  },
+
+  sendReminder: (userId, seriesId, unreadChapters) => {
+    // Send reminder to user about the unread chapters for the series
+  }
+};
+
+module.exports = notifications;
